68c2f3404c126d7c73274c175b0f34259722622e
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-06-21 17:50:29 +0200

    added-if-and-else

--- js/script.js
+++ js/script.js
@@ -14,12 +14,29 @@ L'output del prezzo finale va stampato in pagina forma umana (con massimo due de
 
 const kilometers = parseInt (prompt ('Quanti Km vuoi percorrere?' , '1000'));
 const age = parseInt (prompt('Quanti anni hai?' , '17'));
-const ticketPrice = parseInt ('0.21' * kilometers);
-const underAgePrize = '20';
-const overPrize = '40'; 
-let result = Math.round((ticketPrice / underAgePrize) * 100);
-let result = Math.round((ticketPrice / overPrize) * 100);
+const ticketPrice =  0.21 * kilometers   ;
+const discountOver =  (ticketPrice * 40) / 100   ;
+const discountYoung =  (ticketPrice * 20) / 100  ;
+const finalPrizeOver = ticketPrice - discountOver;
+const finalPrizeYoung = ticketPrice - discountYoung;
+const prize = document.getElementById('prize');
+const sentence = 'il prezzo del tuo biglietto è : ';
+ 
+ 
 
 console.log('scelta utente : ' +  kilometers);
 console.log('anni utente : ' + age );
 console.log('prezzo biglietto : ' + ticketPrice);
+console.log('sconto over : ' + discountOver);
+console.log('sconto young : ' + discountYoung);
+/*console.log('prezzo scontato over : ' + finalPrizeOver);
+console.log('prezzo scontato young : ' + finalPrizeYoung);*/
+
+if (age < 18 ) {
+    prize.innerText = `${sentence} ${finalPrizeYoung} `;
+}
+
+else if (age > 65) {
+    prize.innerText = `${sentence} ${finalPrizeOver} `;
+}
+
